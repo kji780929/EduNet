13306d17a525e568f6b08e498d67cd008a36cc0d
kji780929 <kji780929@hanmail.net>
2014-04-11 17:42:51 +0900

    Project Start

---
+++ EduNet/Controllers/CourseController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace EduNet.Controllers
+{
+    public class CourseController : Controller
+    {
+        public ActionResult Elementary()
+        {
+            ViewBag.Message = "초등학교 교과";
+
+            return View();
+        }
+
+        public ActionResult Middle()
+        {
+            ViewBag.Message = "중학교 교과";
+
+            return View();
+        }
+
+        public ActionResult High()
+        {
+            ViewBag.Message = "고등학교 교과";
+
+            return View();
+        }
+
+    }
+}
---
+++ EduNet/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EduNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "EduNet에 오신 것을 환영합니다.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
